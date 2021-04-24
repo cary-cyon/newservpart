f57a61c26265f38b3a470b76afdef3bb2350a433
cary-cyon <kozvlad99@gmail.com>
2021-04-24 18:42:11 +0300

    Add Producter

--- AshanContext.cs
+++ AshanContext.cs
@@ -7,14 +7,18 @@ using System.Threading.Tasks;
 
 namespace servPart
 {
-    public class AshanContext: DbContext
+    public class AshanContext : DbContext
     {
-        public AshanContext(DbContextOptions<AshanContext> options):base(options)
+        public AshanContext(DbContextOptions<AshanContext> options) : base(options)
         {
 
         }
         public DbSet<Product> Products { get; set; }
         public DbSet<TypeProduct> Types { get; set; }
         public DbSet<ProductQrcode> ProductQrcodes { get; set; }
+        public DbSet<Producter> Producters { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserClass> UserClasses {get; set;}
+        public DbSet<UserType> UserTypes { get; set; }
     }
 }
